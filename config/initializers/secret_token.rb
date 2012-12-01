60c7f031ac5725cffed4f350d80f46f9eaf21c63
Julian Kelabora <jkelabora@dius.com.au>
2012-12-01 13:30:20 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhokPlanGenerator::Application.config.secret_token = '18214da555249ea2650338043dbf25710694ef3ba4558f20f51904b2f73c7bede1d7e88a052a0670810aed81af024f8a95399d432064bcb5e304896ce722bb69'
