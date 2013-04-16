a6a326ddb6fb758c7387ffde0eff8a58de67300b
Julian Kelabora <jkelabora@dius.com.au>
2013-04-16 23:30:09 +1000

    inefficient query to find out all the people to send notifications to given a triggered event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,38 +14,35 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @task = @event.tasks.first
-    @person = @task.people.first
-    # mailer = PersonMailer.welcome_email(@event, @person, @task)
-    # @msg = mailer.deliver
-
+    @people = @event.tasks.map{|t| t.people }.flatten.uniq # TODO: THIS IS INEFFICIENT!
     ses = AWS::SimpleEmailService.new(
       # these two env var *names* are preset by Elastic Beastalk and have been given values via the AWS Console
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_KEY']) 
 
-    # only required for sandbox ses usage
-    identity = ses.identities.verify(@person.email)
-    puts "identity.verified? #{identity.verified?}"
+    @people.each do |p|
+      identity = ses.identities.verify(p.email) # only required for sandbox ses usage
+      puts "identity.verified? #{identity.verified?}"
 
+      ses.send_email(
+        :subject => "A Sample Email #{Time.now}",
+        :from => 'jkelabora@dius.com.au',
+        :to => p.email,
+        :body_text => 'Sample email text.',
+        :body_html => "<h1>Sample Email #{Time.now}</h1>")
+    end
 
-    ses.send_email(
-      :subject => "A Sample Email #{Time.now}",
-      :from => 'jkelabora@dius.com.au',
-      :to => @person.email,
-      :body_text => 'Sample email text.',
-      :body_html => "<h1>Sample Email #{Time.now}</h1>")
-
-
-    @account_sid = 'AC6ef2afcab7f0748e9fc427288cdd5f8d'
-    @auth_token = ENV['PARAM1']
+    account_sid = 'AC6ef2afcab7f0748e9fc427288cdd5f8d'
+    auth_token = ENV['PARAM1']
 
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+    account = client.account
 
-    @account = @client.account
-    @msg = @account.sms.messages.create({:from => '+19402028234', :to =>  @person.mobile, :body => 'rhok-and-roll!'})
-    puts @msg
+    @people.each do |p|
+      @msg = account.sms.messages.create({:from => '+19402028234', :to =>  p.mobile, :body => 'rhok-and-roll!'})
+      puts @msg.inspect
+    end
   end
 
   def edit
