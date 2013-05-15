34b900fb711cbfdde7f2f9b7991fc876fb796d36
Julian Kelabora <jkelabora@dius.com.au>
2013-05-15 19:26:48 +1000

    expand out the email content

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,12 +21,24 @@ class EventsController < ApplicationController
       :secret_access_key => ENV['AWS_SECRET_KEY']) 
 
     @people.each do |p|
+
+      subject = "Active Fire Plan Generator reminder: "
+      subject += "'#{@event.name}' was triggered; you have #{p.tasks.where(event_id: @event).count} related tasks allocated."
+
+      body_text = "Hi #{p.name.capitalize},\n"
+      body_text += "We're sending you this email as a reminder "
+      body_text += "of the #{p.tasks.where(event_id: @event).count} tasks "
+      body_text += "you have allocated to you in your Fire Plan in relation to '#{@event.name}'.\n"
+      body_text += "The tasks are:\n"
+      p.tasks.where(event_id: @event).each {|t| body_text += "- #{t.name}\n"}
+      body_text += "\n\nHope that helps,\nBest regards,\nThe Active Fire Plan Generator team"
+
       ses.send_email(
-        :subject => "A Sample Email #{Time.now}",
+        :subject => subject,
         :from => 'jkelabora@dius.com.au', # ses verified sender address
         :to => p.email,
-        :body_text => 'Sample email text.',
-        :body_html => "<h1>Sample Email #{Time.now}</h1>") if Rails.env == 'production'
+        :body_text => body_text,
+        :body_html => body_text) if Rails.env == 'production'
     end
 
     account_sid = 'AC6ef2afcab7f0748e9fc427288cdd5f8d'
