be533ecae2ad6797dd10e58f0848c310ae2b91c3
Julian Kelabora <jkelabora@dius.com.au>
2013-05-16 21:01:00 +1000

    extract a class for twilio sms sending

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,23 +15,8 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @people = @event.tasks.collect(&:people).flatten.uniq # TODO: THIS IS INEFFICIENT!
-
     EventsMailer.new(@people, @event).deliver_all
-
-    account_sid = 'AC6ef2afcab7f0748e9fc427288cdd5f8d'
-    auth_token = ENV['PARAM1']
-
-    # set up a client to talk to the Twilio REST API
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-    account = client.account
-
-    @people.each do |p|
-      body = "A reminder from the Active Fire Plan Generator. "
-      body += "'#{@event.name}' was triggered; you have #{p.tasks.where(event_id: @event).count} related tasks allocated."
-      body += " Please check #{p.email}" if p.email
-      # sms body can be up to 160 characters long
-      @msg = account.sms.messages.create({:from => '+19402028234', :to => p.mobile, :body => body[0..159]}) if Rails.env == 'production'
-    end
+    TwilioClient.new(@people, @event).deliver_all
   end
 
   def edit
---
+++ app/sms/twilio_client.rb
@@ -0,0 +1,23 @@
+class TwilioClient
+
+  def initialize people, event
+    @people = people
+    @event = event
+    account_sid = 'AC6ef2afcab7f0748e9fc427288cdd5f8d'
+    auth_token = ENV['PARAM1']
+
+    # set up a client to talk to the Twilio REST API
+    @account = Twilio::REST::Client.new(account_sid, auth_token).account
+  end
+
+  def deliver_all
+    @people.each do |p|
+      body = "A reminder from the Active Fire Plan Generator. "
+      body += "'#{@event.name}' was triggered; you have #{p.tasks.where(event_id: @event).count} related tasks allocated."
+      body += " Please check #{p.email}" if p.email
+      # sms body can be up to 160 characters long
+      @account.sms.messages.create({:from => '+19402028234', :to => p.mobile, :body => body[0..159]}) if Rails.env == 'production'
+    end
+  end
+
+end
