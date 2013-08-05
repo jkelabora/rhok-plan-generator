6a1286b68b1340bb54607af923a753123f501ff7
George Papas <george.papas@gmail.com>
2013-08-05 14:20:56 +1000

    Removed entire auto generated test directory

--- test/unit/person_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PersonTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Person.new.valid?
-  end
-end
--- test/unit/plan_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlanTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Plan.new.valid?
-  end
-end
