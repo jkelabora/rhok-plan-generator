dc9a64079e099cb9434f855bad72caac720a0c29
Julian Kelabora <jkelabora@dius.com.au>
2013-08-20 00:51:55 +1000

    allow newly created blank plans to be kept private

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,15 +1,23 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :name, :postcode, :public_guid, :private_guid
+  attr_accessible :name, :postcode, :public_guid, :private_guid, :opt_out
 
   has_many :tasks
   has_many :people
 
-  before_create :init
+  before_create :init_guids
+
+  def opt_out
+    @opt_out
+  end
+
+  def opt_out= transient_check_box_flag
+    @opt_out = transient_check_box_flag
+  end
 
   protected
-    def init
-      self.private_guid ||= SecureRandom.hex 32
-      self.public_guid  ||= SecureRandom.hex 32
+    def init_guids
+      self.private_guid = SecureRandom.hex 32
+      self.public_guid = SecureRandom.hex 32 unless self.opt_out == '1'
     end
 
 end
