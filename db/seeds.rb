ecddbb9ebfeef8bf35da26aef8a006979c3cfd2c
Ken Ong <kenming.ong@gmail.com>
2013-10-19 17:32:55 +1100

    Making DB Seed not clear out the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,37 @@
 
-# Event.delete_all
-# Task.delete_all
-# Person.delete_all
+Event.find_or_create_by_name_and_priority('Before the bushfire season', 10)
+Event.find_or_create_by_name_and_priority('During the bushfire season', 20)
+Event.find_or_create_by_name_and_priority('Night before you evacuate/defend', 30)
 
-# Event.new(name: 'Before the bushfire season', priority: 10).save!
-# Event.new(name: 'During the bushfire season', priority: 20).save!
-# Event.new(name: 'Night before you evacuate/defend', priority: 30).save!
+event = Event.find_by_name('Before the bushfire season')
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Clear fine fuels around the house/property - weeds, leaf litter, dead branches (smaller than diameter of a pencil).', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Trim tree branches overhanging the house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Trim shrubs around the house - no shrubs over 1 metre.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Clear gutters.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Cover air vents with fine metal mesh.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Check hose fittings are in good working order.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Check hoses reach entire house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Prepare emergency stay/go kit.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Place emergency stay/go kit in an easy accessible location.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Move firewood away from the house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Move any flammable items away from the house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Back up computer files onto external hard drive.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Know where metal buckets, mops, rakes, etc are located.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Prepare relocation kit.', event.id)
 
-# event = Event.find_by_name('Before the bushfire season')
-# Task.new(custom: false, name: 'Clear fine fuels around the house/property - weeds, leaf litter, dead branches (smaller than diameter of a pencil).', event_id: event.id).save!
-# Task.new(custom: false, name: 'Trim tree branches overhanging the house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Trim shrubs around the house - no shrubs over 1 metre.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Clear gutters.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Cover air vents with fine metal mesh.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Check hose fittings are in good working order.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Check hoses reach entire house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Prepare emergency stay/go kit.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Place emergency stay/go kit in an easy accessible location.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Move firewood away from the house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Move any flammable items away from the house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Back up computer files onto external hard drive.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Know where metal buckets, mops, rakes, etc are located.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Prepare relocation kit.', event_id: event.id).save!
+event = Event.find_by_name('During the bushfire season')
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Fill external water sources - eg. wheelie bins with lids.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Maintain fine fuels around the house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Keep gutters clear.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Know where metal buckets, mops, rakes, etc are located.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Know where boots are for all household members.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Practice moving through the house in full protective gear in the complete dark.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Find somewhere safe to put photos, scrapbooks, documents (birth certificates, insurance docs & receipts, passports).', event.id)
 
-# event = Event.find_by_name('During the bushfire season')
-# Task.new(custom: false, name: 'Fill external water sources - eg. wheelie bins with lids.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Maintain fine fuels around the house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Keep gutters clear.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Know where metal buckets, mops, rakes, etc are located.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Know where boots are for all household members.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Practice moving through the house in full protective gear in the complete dark.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Find somewhere safe to put photos, scrapbooks, documents (birth certificates, insurance docs & receipts, passports).', event_id: event.id).save!
-
-# event = Event.find_by_name('Night before you evacuate/defend')
-# Task.new(custom: false, name: 'Listen to ABC 774 for updates on fire conditions.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Check warrandyte.org.au Real Time Info page regularly for updates on fire & weather conditions.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Charge mobile phones and i-pods.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Check emergency stay/go kit is complete.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Move door mats and other loose flammable items inside or away from the house.', event_id: event.id).save!
-# Task.new(custom: false, name: 'Put emergency stay/go kit in car or near the front door.', event_id: event.id).save!
+event = Event.find_by_name('Night before you evacuate/defend')
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Listen to ABC 774 for updates on fire conditions.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Check warrandyte.org.au Real Time Info page regularly for updates on fire & weather conditions.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Charge mobile phones and i-pods.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Check emergency stay/go kit is complete.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Move door mats and other loose flammable items inside or away from the house.', event.id)
+Task.find_or_create_by_custom_and_name_and_event_id(false, 'Put emergency stay/go kit in car or near the front door.', event.id)
