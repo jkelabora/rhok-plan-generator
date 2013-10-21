421dd5959d88252f254bf7c1bdc214044fe8a999
Julian Kelabora <jkelabora@dius.com.au>
2013-10-22 00:47:44 +1100

    cut-back the allocations page styling and markup to get the basics in better shape for re-styling

--- app/assets/javascripts/allocations/allocations.js
+++ app/assets/javascripts/allocations/allocations.js
@@ -1,68 +1,59 @@
-// disable Moderinizr for now... need to sort out asset compilation
 
-// if (Modernizr.draganddrop) {
-  // Browser supports HTML5 DnD.
-
-  function createAllocation(task_id, person_id) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/allocations', true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onload = function(e) {
-      if (this.status == 200) {
-        var updated_allocations = JSON.parse(this.responseText);
-        elem = jQuery("ul#people [data-id="+updated_allocations.person_id+"] ul.allocations");
-        var html = "";
-        for (var a in updated_allocations.allocations) {
-          html += "<li class='allocation'><span class='allocation' data-id='" + updated_allocations.allocations[a].id + "'>";
-          html += updated_allocations.allocations[a].name.substr(0, 40);
-          html += "</span></li>";
-        }
-        jQuery(elem).html(html);
-        jQuery(elem).find('span').bind('click', deleteAllocation);
+function createAllocation(task_id, person_id) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/allocations', true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      var updated_allocations = JSON.parse(this.responseText);
+      elem = jQuery("ul#allocations");
+      var html = "";
+      for (var a in updated_allocations.allocations) {
+        html += "<li class='allocation' data-id='" + updated_allocations.allocations[a].id + "'>";
+        html += updated_allocations.allocations[a].name;
+        html += "</li>";
       }
-    };
-    current_event_id = jQuery('ul#events li.active').attr('data-id');
-    xhr.send(JSON.stringify({id: current_event_id, allocation: {task_id: task_id, person_id: person_id}}));
+      jQuery(elem).html(html);
+      jQuery(elem).find('li').bind('click', deleteAllocation);
+    }
+  };
+  current_event_id = jQuery('ul#events li.active').attr('data-id');
+  xhr.send(JSON.stringify({id: current_event_id, allocation: {task_id: task_id, person_id: person_id}}));
+}
+
+function deleteAllocation(e){
+    var elem = this;
+    if(confirm("Are you sure?")){
+      $.ajax({
+        url: '/allocations/' + elem.getAttribute('data-id'),
+        type: 'post',
+        dataType: 'script',
+        data: { '_method': 'delete' },
+        success: function() {
+          $(elem).remove();
+        }
+      });
+      return false;
+    } else {
+      return false;
+    }
   }
 
-  function deleteAllocation(e){
-      var elem = this;
-      if(confirm("Are you sure?")){
-        $.ajax({
-          url: '/allocations/' + elem.getAttribute('data-id'),
-          type: 'post',
-          dataType: 'script',
-          data: { '_method': 'delete' },
-          success: function() {
-            $(elem).parent().remove();
-          }
-        });
-        return false;
-      } else {
-        return false;
-      }
-    }
-
-  // another bit of jquery blah-ness
-  jQuery(function($) { // document ready
-    $('ul.allocations span').bind('click', deleteAllocation);
-    $('ul#tasks li').draggable({
-      opacity: 0.7, helper: "clone",
-      appendTo: "body"
-    });
-    $( "div.person" ).droppable({
-      activeClass: "ui-state-default",
-      hoverClass: "ui-state-hover",
-      accept: ":not(.ui-sortable-helper)",
-      drop: function( event, ui ) {
-        $( "<li class='allocation'></li>" ).text( ui.draggable.text() ).appendTo( $( this ).parent().find("allocations") );
-        task_id = ui.draggable.find("span").attr("data-id");
-        person_id = $( this ).parent().get(0).getAttribute('data-id');
-        createAllocation(task_id, person_id);
-      }
-    });
+jQuery(function($) { // document ready
+  $('ul#allocations li').bind('click', deleteAllocation);
+  $('ul#tasks li').draggable({
+    opacity: 0.7, helper: "clone",
+    appendTo: "body"
   });
-
-// } else {
-  // Fallback to a library solution for drag and drop.
-// }
+  $( "ul#allocations" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+      task_id = ui.draggable.attr("data-id");
+      $( "<li class='allocation' data-id="+task_id+" ></li>" ).text( ui.draggable.text() ).appendTo( $( "ul.allocations") );
+      person_id = $( "span.person" ).attr('data-id');
+      createAllocation(task_id, person_id);
+    }
+  });
+});
