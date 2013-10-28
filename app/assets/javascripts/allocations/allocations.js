98a20709e4323fd27269e1a621fa1caa42de98b6
Julian Kelabora <jkelabora@dius.com.au>
2013-10-29 00:36:56 +1100

    getting all allocation functionality back after re-styling

--- app/assets/javascripts/allocations/allocations.js
+++ app/assets/javascripts/allocations/allocations.js
@@ -6,13 +6,23 @@ function createAllocation(task_id, person_id) {
   xhr.onload = function(e) {
     if (this.status == 200) {
       var resp = JSON.parse(this.responseText);
-      $( "<li class='allocation' data-id="+resp.allocation_id+" >"+resp.task_name+"</li>" ).appendTo( $( "ul#allocations") );
-      $('ul#allocations').find('li#add').appendTo('ul#allocations'); // move the Add to the end of the list..
-      $('li#add').text('Add item'); // move the Add to the end of the list..
-      $("li.allocation[data-id='"+resp.allocation_id+"']").bind('click', deleteAllocation);
+      var position = $('table#allocations tbody > tr').length.toString();
+      var padded_position = position.length === 1 ? '0' + position : position;
+      var html = ""
+        html +="<tr>";
+        html +=  "<td class='allocation-signature'><h4>"+padded_position+"</h4></td>"
+        html +=  "<td>"+resp.task_name+"</td>";
+        html +=  "<td class='allocation' data-id="+resp.allocation_id+"><h4 class='delete'>x</h4></td>"
+        html +="</tr>"
+      $( html ).appendTo( $( "table#allocations") );
+
+      $('table#allocations').find('tr#add-row').appendTo('table#allocations'); // move the Add to the end of the list..
+      $('td#add').text('Click here to add'); // work-around bad binding
+
+      $("td.allocation[data-id='"+resp.allocation_id+"']").bind('click', deleteAllocation);
     }
   };
-  current_event_id = $('ul#events li.active').attr('data-id');
+  current_event_id = $('dl.sub-nav dd.active').attr('data-id');
   xhr.send(JSON.stringify({id: current_event_id, allocation: {task_id: task_id, person_id: person_id}}));
 }
 
@@ -25,7 +35,7 @@ function deleteAllocation(e){
         dataType: 'script',
         data: { '_method': 'delete' },
         success: function() {
-          $(elem).remove();
+          $(elem.parentNode).remove();
         }
       });
       return false;
@@ -47,33 +57,48 @@ $(function($) { // document ready
     indicator : 'Saving...',
     name      : 'postcode'
   });
+
+
   $('.edit#add').editable('/allocations', {
     indicator  : 'Saving...',
     name       : 'name',
     data       : ' ', // empty string doesn't work unfortunately
     submitdata : function(value, settings) {
       var person_id = $('span#person-id').attr('data-id');
-      var event_id  = $('ul#events li.active').attr('data-id');
+      var event_id  = $('dl.sub-nav dd.active').attr('data-id');
       return {person_id: person_id, event_id: event_id};
     },
     callback : function(value, settings) {
       var resp = JSON.parse(value);
-      $( "<li class='allocation' data-id="+resp.allocation_id+" >"+resp.task_name+"</li>" ).appendTo( $( "ul#allocations") );
-      $('ul#allocations').find('li#add').appendTo('ul#allocations'); // move the Add to the end of the list..
-      $('li#add').text('Add item'); // work-around bad binding
-      $("li.allocation[data-id='"+resp.allocation_id+"']").bind('click', deleteAllocation);
+
+      var position = $('table#allocations tbody > tr').length.toString();
+      var padded_position = position.length === 1 ? '0' + position : position;
+      var html = ""
+        html +="<tr>";
+        html +=  "<td class='allocation-signature'><h4>"+padded_position+"</h4></td>"
+        html +=  "<td>"+resp.task_name+"</td>";
+        html +=  "<td class='allocation' data-id="+resp.allocation_id+"><h4 class='delete'>x</h4></td>"
+        html +="</tr>"
+      $( html ).appendTo( $( "table#allocations") );
+
+      $('table#allocations').find('tr#add-row').appendTo('table#allocations'); // move the Add to the end of the list..
+      $('td#add').text('Click here to add'); // work-around bad binding
+
+      $("td.allocation[data-id='"+resp.allocation_id+"']").bind('click', deleteAllocation);
      }
   });
   // END
 
-  $('li.allocation').bind('click', deleteAllocation);
+  $('td.allocation').bind('click', deleteAllocation);
 
-  $('ul#tasks li').draggable({
+  $('table#tasks td.suggested').draggable({
     opacity: 0.7, helper: "clone",
-    appendTo: "body"
+    appendTo: "body",
+    cursor: "crosshair",
+    cursorAt: { top: 0 }
   });
 
-  $( "ul#allocations" ).droppable({
+  $( "table#allocations" ).droppable({
     activeClass: "ui-state-default",
     hoverClass: "ui-state-hover",
     accept: ":not(.ui-sortable-helper)",
