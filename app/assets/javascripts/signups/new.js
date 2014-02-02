9ee8a214caffee3b11f4ae2dbef92e4e6293153d
Julian Kelabora <jkelabora@dius.com.au>
2014-02-02 20:02:06 +1100

    bit of cleanup

--- app/assets/javascripts/signups/new.js
+++ app/assets/javascripts/signups/new.js
@@ -1,9 +1,3 @@
 $( "div.questions input" ).click(function() {
   $( this ).parent().parent().parent().toggleClass( "highlight" );
 });
-
-// $( "div.questions input" ).click(function() {
-//   $( "div.questions" ).toggle(function() {
-      
-//   });
-// });
