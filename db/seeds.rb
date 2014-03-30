ac20accbb81f33e0d06bdcc48e9d78096f185399
Julian Kelabora <jkelabora@dius.com.au>
2014-03-30 17:12:17 +1100

    disable OnceOffTemplateLoad on seed for now as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require_relative './data_tasks'
 
 # will exit if plan already exists (in any form)
-OnceOffTemplateLoad.new.load_template_plan 'Warrandyte Template', '3113'
+# OnceOffTemplateLoad.new.load_template_plan 'Warrandyte Template', '3113'
 
 # plans (by id) to preserve
 # WhiteListedDataDestroyer.new.clear_out_all_plans_and_data_except_for_these_plans do
