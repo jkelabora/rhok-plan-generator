249351827f8415b883219c980bcb61347bb68d20
Julian Kelabora <jkelabora@dius.com.au>
2014-04-26 13:50:23 +1000

    better description of links in email, and include read-only show plan link

--- app/helpers/mailto_helper.rb
+++ app/helpers/mailto_helper.rb
@@ -5,7 +5,23 @@ module MailtoHelper
   end
 
   def share_mailto plan
-    "mailto:?Subject=Link%20to%20the%20plan%20%27#{plan.escaped_name}%27&Body=#{plan_edit_url(:private_guid => plan.private_guid)}"
+    [
+      "mailto:?",
+      "Subject=Links for the plan %27#{plan.escaped_name}%27&",
+      "Body=",
+      "To return to the plan customisation page use:",
+      crlf,
+      "#{plan_edit_url(:private_guid => plan.private_guid)}",
+      crlf, crlf,
+      "or to share a read-only view of your plan with someone else send them:",
+      crlf,
+      "#{plan_url(:public_guid => plan.public_guid)}",
+      crlf, crlf
+    ].join.gsub(' ', space)
   end
+
+  private
+  def crlf;  "%0D%0A"; end
+  def space; "%20";    end
 
 end
