74e0e471f553ec7a6d985f6075ac098bcf35ae42
Julian Kelabora <jkelabora@dius.com.au>
2014-05-01 21:03:33 +1000

    working idempotent template plan loader invoked via db:seed

--- db/data_tasks.rb
+++ db/data_tasks.rb
@@ -29,13 +29,25 @@ class OnceOffTemplateLoad
     end
 
   # designed to only persist data once (despite subsequent executions) to preserve updates via ActiveAdmin
-    def load_template_plan
+    def load
 
         return unless Plan.where(name: name, postcode: postcode).empty?
 
+        setup_events
         require 'csv'
         Dir.glob(File.join(Rails.root,"fixtures/#{name_as_directory}/*")).each do |filename|
+            load_all_tasks_and_tag_for filename
+        end
+        setup_the_plan
+    end
 
+    private
+
+    def find_matching_event filename
+        event_name = filename.match(/.*JC - (.*)\.csv/)[1]
+        event = Event.find_by_name event_name
+        raise "couldn't find matching event for #{filename}" unless event
+        event
     end
 
     def events
@@ -51,64 +63,63 @@ class OnceOffTemplateLoad
         events << Event.find_or_create_by_name("Relocation Kit")
         events << Event.find_or_create_by_name("Emergency Kit")
         events << Event.find_or_create_by_name("Emergency Contact Numbers")
-
     end
 
-def name_as_directory
-    name.downcase.gsub(' ','-')
-end
-
-
-# loose match filename to event mapping
-# map to an event
-
-# per row
-
-# --->create the plan from the tasks against each event
-
-
-def generally_applicable_task? row
-  # example row ["Items of special value", "√", "√", "√", "√", "√", "√"]
-  row[1..6].reduce(true){ |c,e| c && e == "√" }
-end
-
-ORDERED_TAGS = [:alone, :not_alone, :children, :not_children, :pets, :not_pets]
-def apply_tags_to task
-  ORDERED_TAGS.each_with_index do |tag,idx|
-    (task.tag_list.add(tag.to_s); t.save) if task[idx] == "√"
-  end
-end
-
-def do_the_work filename, event
-    CSV.read(filename).each do |row|
-      task = Task.find_or_create_by_custom_and_name_and_event_id(false, row[0], event.id)
-      next if generally_applicable_task? row
-      apply_tags_to task
+    def name_as_directory
+        name.downcase.gsub(' ','-')
     end
-end
 
+    def generally_applicable_task? row
+      # example row ["Items of special value", "Y", "Y", "Y", "Y", "Y", "Y"]
+      row[1..6].reduce(true){ |c,e| c && e == "Y" }
+    end
 
-# create the plan from the tasks against each event
-    @person = Person.anon
-    @person.save
-
-    events.each do |event|
-      tasks = Task.where(event_id: event.id)
-      tasks.each do |original|
-        duplicate = original.dup
-        duplicate.custom = true
-
-        if duplicate.save
-          Allocation.create!(person_id: @person.id, task_id: duplicate.id)
-        end
+    ORDERED_TAGS = [:alone, :not_alone, :children, :not_children, :pets, :not_pets]
+    def apply_tags_to task, row
+      ORDERED_TAGS.each_with_index do |tag,idx|
+        (task.tag_list.add(tag.to_s); task.save) if row[idx+1] == "Y"
       end
     end
 
-    @plan = Plan.new(name: name, postcode: postcode, is_public: true)
-    @plan.save
-    @plan.people << @person
-# end create plan
+    def find_matching_event filename
+        event_name = filename.match(/.*JC - (.*)\.csv/)[1]
+        event = Event.find_by_name event_name
+        raise "couldn't find matching event for #{filename}" unless event
+        event
+    end
 
-  end
+    def create_tasks_and_tag filename, event
+        CSV.read(filename).each do |row|
+          task = Task.find_or_create_by_custom_and_name_and_event_id(false, row[0], event.id)
+          next if generally_applicable_task? row
+          apply_tags_to task, row
+        end
+    end
+
+    def load_all_tasks_and_tag_for filename
+        create_tasks_and_tag filename, find_matching_event(filename)
+    end
+
+    def setup_the_plan
+        anon = Person.anon
+        anon.save
+
+        events.each do |event|
+          tasks = Task.where(event_id: event.id)
+          tasks.each do |original|
+            duplicate = original.dup
+            duplicate.custom = true
+
+            if duplicate.save
+              Allocation.create!(person_id: anon.id, task_id: duplicate.id)
+            end
+          end
+        end
+
+        template_plan = Plan.new(name: name, postcode: postcode, is_public: true)
+        template_plan.save
+        template_plan.people << anon
+    end
 
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require_relative './data_tasks'
 
 # will exit if plan already exists (in any form)
-# OnceOffTemplateLoad.new.load_template_plan 'Warrandyte Template', '3113'
+OnceOffTemplateLoad.new('Warrandyte Template', '3113').load
 
 # plans (by id) to preserve
 # WhiteListedDataDestroyer.new.clear_out_all_plans_and_data_except_for_these_plans do
@@ -9,9 +9,3 @@ require_relative './data_tasks'
 #     Plan.find_by_name_and_postcode('Warrandyte Template', '3113')
 #   ].compact.map(&:id)
 # end
-
-# remove all tags from these tasks
-Task.find_all_by_id([2973,2974,2975,2976,2977,2978]).each do |t|
-  t.tag_list = []
-  t.save
-end
